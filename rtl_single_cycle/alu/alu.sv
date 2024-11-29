800c5eeaf1f49a27a4d22b0355957c43585a13d5
S-mishra07 <sm5423@ic.ac.uk>
2024-11-29 11:45:51 +0000

    added branch instructions and complete ALU operations

--- rtl_single_cycle/alu/alu.sv
+++ rtl_single_cycle/alu/alu.sv
@@ -1,26 +1,44 @@
 module alu #(
-    CONTROL_WIDTH = 3,
-    INPUT_WIDTH = 32
+    parameter CONTROL_WIDTH = 4,
+    parameter DATA_WIDTH = 32
 ) (
-    input logic     [INPUT_WIDTH-1:0]   ALUop1,
-    input logic     [INPUT_WIDTH-1:0]   ALUop2,
+    input logic     [DATA_WIDTH-1:0]   SrcA,
+    input logic     [DATA_WIDTH-1:0]   SrcB,
     input logic     [CONTROL_WIDTH-1:0] ALUctrl,
-    output logic    [INPUT_WIDTH-1:0]   ALUout,
-    output logic                        EQ
+    output logic    [DATA_WIDTH-1:0]   ALUResult,
+    output logic                        Zero
 );
+
+// ALU output into Data Memory
 
     always_comb begin
         case (ALUctrl)
-        3'h0:   ALUout = ALUop1 + ALUop2;
-        3'h1:   ALUout = ALUop1 - ALUop2;
-        3'h2:   ALUout = ALUop1 & ALUop2;
-        3'h3:   ALUout = ALUop1 | ALUop2;   
-        3'h4:   ALUout = 0; 
-        3'h5:   ALUout = ALUop1 < ALUop2 ? 1 : 0;
-        3'h6:   ALUout = 0;
-        3'h7:   ALUout = 0;
+        4'b0000:    ALUResult = SrcA + SrcB;                                   // ADD 
+        4'b0001:    ALUResult = SrcA - SrcB;                                   //SUBTRACT
+        4'b0010:    ALUResult = SrcA ^ SrcB;                                   //XOR
+        4'b0011:    ALUResult = SrcA | SrcB;                                   //OR
+        4'b0100:    ALUResult = SrcA & SrcB;                                   //AND
+        4'b0101:    ALUResult = SrcA << SrcB[4:0];                             // SLL
+        4'b0110:    ALUResult = SrcA >> SrcB[4:0];                             //SRL
+        4'b0111:    ALUResult = $signed(SrcA) >>> SrcB[4:0];                   //SRA
+        4'b1000:    ALUResult = $signed(SrcA) < $signed(SrcB) ? 1 : 0;         // SLT
+        4'b1001:    ALUResult = SrcA < SrcB ? 1 : 0;                           // SLTU
+        default:    ALUResult = '0;
         endcase
-        assign EQ = (ALUop1 == ALUop2);
+    end
+
+// Zero flag
+
+    always_comb begin
+        case (ALUctrl)
+        4'b1010:    Zero = (SrcA == SrcB);                         //BEQ
+        4'b0010:    Zero = (SrcA != SrcB);                         //BNE
+        4'b0110:    Zero = ($signed(SrcA) < $signed(SrcB));        //BLT
+        4'b0111:    Zero = ($signed(SrcA) >= $signed(SrcB));       //BGE
+        4'b1000:    Zero = (SrcA < SrcB);                          //BLTU
+        4'b1001:    Zero = (SrcA >= SrcB);                         //BGEU
+        default:    Zero = 1'b0;
+        endcase
     end
     
 endmodule
