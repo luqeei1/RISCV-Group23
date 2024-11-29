a3f2c7005414b93f8bbadded7949076fcf16223d
TheoShah04 <theodoreshah1@gmail.com>
2024-11-29 11:59:32 +0000

    29/11/24 2

--- ControlUnit/controlUnit.sv
+++ ControlUnit/controlUnit.sv
@@ -4,26 +4,29 @@ module controlUnit #(
     input logic [14:12] funct3,
     input logic funct7,
     input logic zero,
-    output logic PCSrc,
-    output logic ResultSrc,
-    output logic MemWrite,
-    output logic [2:0] ALUControl,
-    output logic ALUSrc,
-    output logic [1:0] IMMSrc,
-    output logic RegWrite
+    output logic JumpD,
+    output logic BranchD,
+    output logic [1:0] ResultSrcD,
+    output logic MemWriteD,
+    output logic [2:0] ALUControlD,
+    output logic ALUSrcD,
+    output logic [1:0] ImmSrcD,
+    output logic RegWriteD
 );
-    logic branch;
     logic [1:0] ALUOp; 
     always_comb begin
         case (op)
-            7'b0000011: RegWrite = 1'b1; ImmSrc = 2'b00; ALUSrc = 1'b1; MemWrite = 1'b0; ResultSrc = 1'b1; Branch = 1'b0; ALUOp = 2'b00; //lw
-            7'b0100011: RegWrite = 1'b0; ImmSrc = 2'b01; ALUSrc = 1'b1; MemWrite = 1'b1; ResultSrc = 1'b0; Branch = 1'b0; ALUOp = 2'b00; //sw
-            7'b0010011: RegWrite = 1'b1; ImmSrc = 2'b00; ALUSrc = 1'b1; MemWrite = 1'b0; ResultSrc = 1'b0; Branch = 1'b0; ALUOp = 2'b10;//I-type
-            7'b0110011: RegWrite = 1'b1; ImmSrc = 2'b00; ALUSrc = 1'b0; MemWrite = 1'b0; ResultSrc = 1'b0; Branch = 1'b0; ALUOp = 2'b10; //R-type
-            7'b1100011: RegWrite = 1'b0; ImmSrc = 2'b10; ALUSrc = 1'b0; MemWrite = 1'b0; ResultSrc = 1'b0; Branch = 1'b1; ALUOp = 2'b01; //B-type
-            default: RegWrite = 1'b0; ImmSrc = 2'b00; ALUSrc = 1'b1; MemWrite = 1'b0; ResultSrc = 1'b1; Branch = 1'b0; ALUOp = 2'b11; //Invalid
+            7'b0110011: RegWriteD = 1'b1; ImmSrcD = 3'b111; ALUSrcD = 1'b0; MemWriteD = 1'b0; ResultSrcD = 2'b00; BranchD = 1'b0; JumpD = 1'b0; ALUOp = 2'b10; //R-type
+            7'b0010011: RegWriteD = 1'b1; ImmSrcD = 3'b000; ALUSrcD = 1'b1; MemWriteD = 1'b0; ResultSrcD = 2'b00; BranchD = 1'b0; JumpD = 1'b0; ALUOp = 2'b10; //I-type
+            7'b0000011: RegWriteD = 1'b1; ImmSrcD = 3'b000; ALUSrcD = 1'b1; MemWriteD = 1'b0; ResultSrcD = 2'b01; BranchD = 1'b0; JumpD = 1'b0; ALUOp = 2'b00; //Load-type
+            7'b0100011: RegWriteD = 1'b0; ImmSrcD = 3'b001; ALUSrcD = 1'b1; MemWriteD = 1'b1; ResultSrcD = 2'b00; BranchD = 1'b0; JumpD = 1'b0; ALUOp = 2'b00; //S-type
+            7'b1100011: RegWriteD = 1'b0; ImmSrcD = 3'b010; ALUSrcD = 1'b0; MemWriteD = 1'b0; ResultSrcD = 2'b00; BranchD = 1'b1; JumpD = 1'b0; ALUOp = 2'b01; //B-type
+            7'b0110111: RegWriteD = 1'b1; ImmSrcD = 3'b011; ALUSrcD = 1'b1;  //U-type (lui)
+            7'b0010111: ImmSrcD = 3'b011; //U-type (auipc)
+            7'b1101111: RegWriteD = 1'b1; ImmSrcD = 3'b100; ALUSrcD = 1'b0; MemWriteD = 1'b0; ResultSrcD = 2'b10; BranchD = 1'b0; JumpD = 1'b1; ALUOp = 2'b11; //J-type (jal)
+            7'b1100111: RegWriteD = 1'b1; ImmSrcD = 3'b100; ALUSrcD = 1'b0; MemWriteD = 1'b0; ResultSrcD = 2'b10; BranchD = 1'b0; JumpD = 1'b1; ALUOp = 2'b11; //J-type (jalr)
+            default: RegWriteD = 1'b0; ImmSrcD = 3'b111; ALUSrcD = 1'b1; MemWriteD = 1'b0; ResultSrcD = 2'b11; BranchD = 1'b0; JumpD = 1'b0; ALUOp = 2'b11; //Invalid
         endcase
-    PCSrc = branch && zero
     end
 
     ALUDecoder alu_decoder (
@@ -31,7 +34,7 @@ module controlUnit #(
         .op(op[5]),
         .funct3(funct3),
         .funct7(funct7),
-        .ALUControl(ALUControl)
+        .ALUControlD(ALUControlD)
     );
 
 endmodule
--- ControlUnit/signExtend.sv
+++ ControlUnit/signExtend.sv
@@ -1,15 +1,18 @@
 module signExtend #(
     parameter DATA_WIDTH = 32
 ) (
-    input logic [1:0] IMMSrc,
+    input logic [2:0] ImmSrcD,
     input logic [31:7] ImmInput,
-    output logic [DATA_WIDTH-1:0] ImmExt 
+    output logic [DATA_WIDTH-1:0] ImmExtD 
 );
     always_comb begin
-        case (IMMSrc)
-            2'b00: ImmExt = {{20{ImmInput[31]}}, ImmInput[31:20]}; //I-type
-            2'b01: ImmExt = {{20{ImmInput[31]}}, ImmInput[31:25], ImmInput[11:7]}; //sw
-            2'b10: ImmExt = {{20{ImmInput[31]}}, ImmInput[7], ImmInput[30:25], ImmInput[11:8], 1'b0}; //beq
+        case (ImmSrcD)
+            3'b000: ImmExtD = {{20{ImmInput[31]}}, ImmInput[31:20]}; //I-type
+            3'b001: ImmExtD = {{20{ImmInput[31]}}, ImmInput[31:25], ImmInput[11:7]}; //S-type
+            3'b010: ImmExtD = {{20{ImmInput[31]}}, ImmInput[7], ImmInput[30:25], ImmInput[11:8], 1'b0}; //B-type
+            3'b011: ImmExtD = {{12{ImmInput[31]}}, ImmInput[31:12]}; //U-type
+            3'b100: ImmExtD = {{13{ImmInput[31]}}, ImmInput[19:12], ImmInput[20], ImmInput[30:21]}; //J-type
+            default: 
         endcase
     end
 endmodule
