b58069095e13f23d22bd8a1e2ba7152c2df566dc
S-mishra07 <sm5423@ic.ac.uk>
2024-12-03 22:57:23 +0000

    modified F1_tb.cpp

--- F1_Assembly/F1_tb.cpp
+++ F1_Assembly/F1_tb.cpp
@@ -1,6 +1,6 @@
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-#include "VF1.h"
+#include "Vtop.h"
 #include "vbuddy.cpp"    
 
 #define MAX_SIM_CYC 100000
@@ -12,7 +12,7 @@ int main(int argc, char **argv, char **env) {
 
   Verilated::commandArgs(argc, argv);
   // init top verilog instance
-  VF1 * top = new VF1;
+  Vtop * top = new Vtop;
   // init trace dump
   Verilated::traceEverOn(true);
   VerilatedVcdC* tfp = new VerilatedVcdC;
