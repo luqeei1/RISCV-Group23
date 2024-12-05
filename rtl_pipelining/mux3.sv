c03cca70c034c9c405cc142441fb332b29c71b96
Lucas Venetz <56915052+lucasvenetz@users.noreply.github.com>
2024-12-05 14:59:04 +0000

    Template for muxes

---
+++ rtl_pipelining/mux3.sv
@@ -0,0 +1,19 @@
+module mux3 #(
+    parameter WIDTH = 32
+)(
+    input logic [1:0]         sel,
+    input logic [WIDTH-1:0]     in0,
+    input logic [WIDTH-1:0]     in1,
+    input logic [WIDTH-1:0]     in2,
+    output logic [WIDTH-1:0]    out
+);
+    always_comb begin
+        case (sel)
+            2'b00: out = in0;         
+            2'b01: out = in1;
+            2'b10  out = in2;
+            default:    out = in0;
+        endcase
+    end
+    
+endmodule
