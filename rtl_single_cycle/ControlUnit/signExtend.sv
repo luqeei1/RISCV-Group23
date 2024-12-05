e7a5a9cebef5ae0a7c00bde4d063eae8045fe57c
TheoShah04 <theodoreshah1@gmail.com>
2024-12-05 14:22:53 +0000

    Changed sign extend

--- rtl_single_cycle/ControlUnit/signExtend.sv
+++ rtl_single_cycle/ControlUnit/signExtend.sv
@@ -2,17 +2,18 @@ module signExtend #(
     parameter DATA_WIDTH = 32
 ) (
     input logic [2:0] ImmSrc,
-    input logic [31:7] ImmInput,
+    input logic [DATA_WIDTH-1:0] ImmInput,
     output logic [DATA_WIDTH-1:0] ImmExt
 );
+    input logic [31:7] ImmIn;
     always_comb begin
         case (ImmSrc)
-            3'b000: ImmExt = {{20{ImmInput[31]}}, ImmInput[31:20]}; //I-type
-            3'b001: ImmExt = {{20{ImmInput[31]}}, ImmInput[31:25], ImmInput[11:7]}; //S-type
-            3'b010: ImmExt = {{20{ImmInput[31]}}, ImmInput[7], ImmInput[30:25], ImmInput[11:8], 1'b0}; //B-type
-            3'b011: ImmExt = {ImmInput[31:12], 3'h000}; //U-type
-            3'b100: ImmExt = {{13{ImmInput[31]}}, ImmInput[19:12], ImmInput[20], ImmInput[30:21]}; //jal
-            3'b101: ImmExt = {{20{ImmInput[31]}}, ImmInput[31:20]}; //jalr
+            3'b000: ImmExt = {{20{ImmIn[31]}}, ImmIn[31:20]}; //I-type
+            3'b001: ImmExt = {{20{ImmIn[31]}}, ImmIn[31:25], ImmIn[11:7]}; //S-type
+            3'b010: ImmExt = {{20{ImmIn[31]}}, ImmIn[7], ImmIn[30:25], ImmIn[11:8], 1'b0}; //B-type
+            3'b011: ImmExt = {ImmIn[31:12], 3'h000}; //U-type
+            3'b100: ImmExt = {{13{ImmIn[31]}}, ImmIn[19:12], ImmIn[20], ImmIn[30:21]}; //jal
+            3'b101: ImmExt = {{20{ImmIn[31]}}, ImmIn[31:20]}; //jalr
             default: ImmExt = 32'd0;
         endcase
     end
