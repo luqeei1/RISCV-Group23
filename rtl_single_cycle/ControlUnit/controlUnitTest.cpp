827b5bc45bc0cc1a069c231a9708852460f0e342
TheoShah04 <theodoreshah1@gmail.com>
2024-12-05 16:45:33 +0000

    ControlUnitUnitTest

--- rtl_single_cycle/ControlUnit/controlUnitTest.cpp
+++ rtl_single_cycle/ControlUnit/controlUnitTest.cpp
@@ -19,82 +19,81 @@ protected:
     virtual void TearDown() override {
         delete dut;
     }
-    
-    void clockTick() {
-        dut->clk = 1;
-        evaluate();
-        dut->clk = 0;
-        evaluate();
-    }
 
     void evaluate() {
         dut->eval(); 
     }
 };
 
- TEST_F(controlUnitTest, R-type) {
-    dut->Instr = 0x00;   
+ // Test for R-type instruction
+TEST_F(controlUnitTest, RTypeInstruction) {
+    dut->Instr = 0b00000000000100001000000010110011;  // Example R-type instruction
     evaluate();
-    clockTick();
-    evaluate();
-    EXPECT_EQ(dut->PCSrc, 0b00); 
-    EXPECT_EQ(dut->ResultSrc, 0x000000020); 
-    EXPECT_EQ(dut->MemWrite, 0x000000020); 
-    EXPECT_EQ(dut->ALUControl, 0x000000020); 
-    EXPECT_EQ(dut->ALUSrc, 0x000000020); 
-    EXPECT_EQ(dut->ImmSrc, 0x000000020); 
-    EXPECT_EQ(dut->RegWrite, 0x000000020); 
-    EXPECT_EQ(dut->modeBU, 0x000000020); 
-} 
-
-TEST_F(data_memoryTest, WriteEnableIsOneAndResultSrcIsOne) {
-    dut->ResultSrc = 1;   
-    dut->A = 0x000000;
-    dut->WE = 1;
-    dut->WD = 0x0002;
-    dut->modeBU = 1;
-    evaluate();
-    clockTick();
-    evaluate();
-    EXPECT_EQ(dut->Result, 0x0002); 
+    EXPECT_EQ(dut->PCSrc, 0b00);
+    EXPECT_EQ(dut->ResultSrc, 0b00);
+    EXPECT_EQ(dut->MemWrite, 0b0);
+    EXPECT_EQ(dut->ALUCtrl, 0b0000);  // Assuming ALU performs ADD operation
+    EXPECT_EQ(dut->ALUSrc, 0b0);
+    EXPECT_EQ(dut->ImmSrc, 0b111);
+    EXPECT_EQ(dut->RegWrite, 0b1);
+    EXPECT_EQ(dut->modeBU, 0b000);  // Default invalid for non-load/store
 }
 
-TEST_F(data_memoryTest, WriteEnableIsOneAndResultSrcIsOneAndModeBUIsFour) {
-    dut->ResultSrc = 1;   
-    dut->A = 0x000040;
-    dut->WE = 1;
-    dut->WD = 0x0004;
-    dut->modeBU = 4;
+// Test for I-type instruction
+TEST_F(controlUnitTest, ITypeInstruction) {
+    dut->Instr = 0b00000000000100001000000010010011;  // Example I-type instruction
     evaluate();
-    clockTick();
-    evaluate();
-    EXPECT_EQ(dut->Result, 0x0004); 
+    EXPECT_EQ(dut->PCSrc, 0b00);
+    EXPECT_EQ(dut->ResultSrc, 0b00);
+    EXPECT_EQ(dut->MemWrite, 0b0);
+    EXPECT_EQ(dut->ALUCtrl, 0b0000);  // Assuming ALU performs ADDI operation
+    EXPECT_EQ(dut->ALUSrc, 0b1);
+    EXPECT_EQ(dut->ImmSrc, 0b000);
+    EXPECT_EQ(dut->RegWrite, 0b1);
+    EXPECT_EQ(dut->modeBU, 0b000);
 }
 
-TEST_F(data_memoryTest, WriteEnableIsOneAndResultSrcIsOneAndModeBUIsThree) {
-    dut->ResultSrc = 1;   
-    dut->A = 0x000050;
-    dut->WE = 1;
-    dut->WD = 0x00FF;
-    dut->modeBU = 3;
+// Test for Load instruction
+TEST_F(controlUnitTest, LoadInstruction) {
+    dut->Instr = 0b00000000000100001000000010000011;  // Example Load instruction
     evaluate();
-    clockTick();
-    evaluate();
-    EXPECT_EQ(dut->Result, 0xFFFFFFFF); 
+    EXPECT_EQ(dut->PCSrc, 0b00);
+    EXPECT_EQ(dut->ResultSrc, 0b01);  // Result from memory
+    EXPECT_EQ(dut->MemWrite, 0b0);
+    EXPECT_EQ(dut->ALUCtrl, 0b0000);  // Load doesn't affect ALU control
+    EXPECT_EQ(dut->ALUSrc, 0b1);
+    EXPECT_EQ(dut->ImmSrc, 0b000);
+    EXPECT_EQ(dut->RegWrite, 0b1);
+    EXPECT_EQ(dut->modeBU, 0b011);  // Load byte mode
 }
 
-TEST_F(data_memoryTest, WriteEnableIsOneAndResultSrcIsOneAndModeBUIsFive) {
-    dut->ResultSrc = 1;   
-    dut->A = 0x000000;
-    dut->WE = 1;
-    dut->WD = 0x0004;
-    dut->modeBU = 5;
+// Test for S-type instruction
+TEST_F(controlUnitTest, STypeInstruction) {
+    dut->Instr = 0b00000000000100001000001010100011;  // Example Store instruction
     evaluate();
-    clockTick();
-    evaluate();
-    EXPECT_EQ(dut->Result, 0x0004); 
+    EXPECT_EQ(dut->PCSrc, 0b00);
+    EXPECT_EQ(dut->ResultSrc, 0b00);
+    EXPECT_EQ(dut->MemWrite, 0b1);
+    EXPECT_EQ(dut->ALUCtrl, 0b0000);  // Store doesn't affect ALU control
+    EXPECT_EQ(dut->ALUSrc, 0b1);
+    EXPECT_EQ(dut->ImmSrc, 0b001);
+    EXPECT_EQ(dut->RegWrite, 0b0);
+    EXPECT_EQ(dut->modeBU, 0b011);  // Store byte mode
 }
 
+// Test for J-type instruction
+TEST_F(controlUnitTest, JTypeInstruction) {
+    dut->Instr = 0b00000000000100001000000111101111; // Example JAL instruction
+    evaluate();
+    EXPECT_EQ(dut->PCSrc, 0b01);
+    EXPECT_EQ(dut->ResultSrc, 0b10);  // Result from PC+4
+    EXPECT_EQ(dut->MemWrite, 0b0);
+    EXPECT_EQ(dut->ALUCtrl, 0b1111);  // Doesn't use ALU
+    EXPECT_EQ(dut->ALUSrc, 0b0);
+    EXPECT_EQ(dut->ImmSrc, 0b100);
+    EXPECT_EQ(dut->RegWrite, 0b1);
+    EXPECT_EQ(dut->modeBU, 0b000);
+}
 
 int main(int argc, char **argv)
 {
