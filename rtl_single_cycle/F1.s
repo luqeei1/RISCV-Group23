80bfa24b7c0a1bf9e172ec27ca981aa3bcfaa19f
S-mishra07 <sm5423@ic.ac.uk>
2024-12-06 14:12:05 +0000

    changed assembly for a simple check

--- rtl_single_cycle/F1.s
+++ rtl_single_cycle/F1.s
@@ -2,24 +2,15 @@
 .globl main
 
 main:
-    li      t2, 0x2000
-iloop:
-    lw      s10, 0 (t2)
-    beq     s10, zero, iloop
-    jal     ra, init
-    j       iloop
-    ret
-
-init:
-    addi    a0, zero, 0x1
-    addi    a0, zero, 0x3
-    addi    a0, zero, 0x7
-    addi    a0, zero, 0xF
-    addi    a0, zero, 0x1F
-    addi    a0, zero, 0x3F
-    addi    a0, zero, 0x7F
-    addi    a0, zero, 0xFF
-    ret
+    addi    a0, zero, 0x0                # a0 = 0         -  Value at State 0
+    addi    t1, zero, 0b11111111         # a1 = 11111111  -  Value at State 8
+shift_loop:
+    slli    a0, a0, 1
+    ori     a0, a0, 0b1                  # Shift left and replace the gap with a 1 (increment states)
+    bne     a0, t1, shift_loop           # if a0 != 1111 1111 (State 8) then reset to 0 
+reset_loop: 
+    addi    a0, zero, 0x0                # Reset to state 0 if at State 8
+    beq     zero, zero, shift_loop       # unconditional branch to restart the process
 
 
 
