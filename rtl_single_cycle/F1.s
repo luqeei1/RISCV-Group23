6a3dc8751fcbf20cca8e0f81e61715fbc38978dc
S-mishra07 <sm5423@ic.ac.uk>
2024-12-06 13:42:18 +0000

    changed assembly

--- rtl_single_cycle/F1.s
+++ rtl_single_cycle/F1.s
@@ -1,50 +1,25 @@
-# s0: current state
-# s1: target value
-# s2: output state
-# s3: delay counter
-# t0: temporary register
-
 .text
 .globl main
 
 main:
-    JAL ra, init
-    j   main
+    li      t2, 0x2000
+iloop:
+    lw      s10, 0 (t2)
+    beq     s10, zero, iloop
+    jal     ra, init
+    j       iloop
+    ret
 
 init:
-    lui t0, 0x0
-    addi t0, t0, 0x0
-    si t0, 0x0
-
-    lui t0, 0x0
-    addi t0, t0, 0xff
-    si t0, 0x4
-
-    lui t0, 0x0
-    addi t0, t0, 0x0
-    si t0, 0x8
-
-    lui t0, 0x0
-    addi t0, t0, 0x4
-    si t0, 0xc
-
-    li s0, 0x0
-    li s1, 0x4 
-    li s2, 0x8
-    li s3, 0xc      
-
-loopi:
-    slli s0, s0, 1 
-    addi s0, s0, 1
-    and s2, s1, s0
-    si s2, 0x8
-
-wait:
-    addi s3, s3, -1 
-    bne s3, zero, wait 
-    lui t0, 0x0
-    addi s3, t0, 0x4 
-    bne s1, s0, loopi
-    addi s2, zero, 0
-    si s2, 0x8
+    addi    a0, zero, 0x1
+    addi    a0, zero, 0x3
+    addi    a0, zero, 0x7
+    addi    a0, zero, 0xF
+    addi    a0, zero, 0x1F
+    addi    a0, zero, 0x3F
+    addi    a0, zero, 0x7F
+    addi    a0, zero, 0xFF
     ret
+
+
+
