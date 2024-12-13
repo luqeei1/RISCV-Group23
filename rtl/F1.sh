59ff8cfbfb50a80a569689df769ef2ffbe79f62b
TheoShah04 <theodoreshah1@gmail.com>
2024-12-13 16:13:33 +0000

    Added script files

---
+++ rtl/F1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Translate Verilog -> C++ including testbench
+verilator   -Wall --trace \
+            --public-flat-rw \
+            -cc top.sv \
+            -top-module top \
+            regfile.sv \
+            instruction_memory.sv \
+            cached_datamem.sv \
+            program_counter.sv \
+            signExtend.sv \
+            alu.sv \
+            BPU.sv \
+            FF_DE.sv \
+            FF_EM.sv \
+            FF_MW.sv \
+            FF_FD.sv \
+            hazard_unit.sv \
+            mux2.sv \
+            mux3.sv \
+            mux4.sv \
+            PC_mux.sv \
+            controlUnit.sv \
+            ALUDecoder.sv \
+            --exe ../F1_pipelined_tb.cpp \
+            --prefix "Vtop" \
+            -o Vtop
+
+# Build C++ project with automatically generated Makefile
+make -j -C obj_dir/ -f Vtop.mk
+
+# Run executable simulation file
+./obj_dir/Vtop
